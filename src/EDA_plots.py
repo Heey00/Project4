b4cb8c39370c156faa443ba43b36ca55e1a7bd1f
Heey00 <102456788+Heey00@users.noreply.github.com>
2022-03-27 15:56:07 -0700

    Update EDA_plots.py
    Changed the function - put all plots on one canvas

--- src/EDA_plots.py
+++ src/EDA_plots.py
@@ -6,26 +6,27 @@ from plot_boxplot import boxplot_plotting
 import matplotlib.pyplot as plt
 
 
-def EDA_plot(train_df):
-    train_df = pd.read_csv(train_df)
+def EDA_plot(train_df, hist_output, boxplot_output):
+    train_df = pd.read_csv(str(train_df))
     X_train = train_df.drop(columns=["class"])
     numeric_looking_columns = X_train.select_dtypes(
         include=np.number).columns.tolist()
     benign_cases = train_df[train_df["class"] == 0]
     malignant_cases = train_df[train_df["class"] == 1]
     #plot histogram
-    for idx, x in enumerate(numeric_looking_columns): 
-        plot_hist_overlay(benign_cases, malignant_cases, 
-                          x, labels=["0 - benign", "1 - malignant"],
-                          fig_no=f"1.{idx}")
-        plt.savefig("../results/"+ x + "_histogram.png")
+    plot_hist_overlay(df0=benign_cases, df1=malignant_cases,
+                 columns=numeric_looking_columns, labels=["0 - benign", "1 - malignant"],
+                 fig_no="1")
+    plt.savefig(str(hist_output))
     #plot boxplot 
     boxplot_plotting(3, 3, 20, 25, numeric_looking_columns, train_df, 2)
-    plt.savefig("../results/eda_boxplots")
+    plt.savefig(str(boxplot_output))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Plots EDA")
-    parser.add_argument("train_df", type=str, help="Path to train_df")
+    parser.add_argument("train_df", help="Path to train_df")
+    parser.add_argument("hist_output", help="Path to histogram output")
+    parser.add_argument("boxplot_output", help="Path to boxplot output")
     args = parser.parse_args()
-    EDA_plot(args.train_df)
+    EDA_plot(args.train_df, args.hist_output, args.boxplot_output)
