8030e6ce50b58d2d0171224018989cde3157328c
Heey00 <102456788+Heey00@users.noreply.github.com>
2022-03-27 16:30:45 -0700

    Update EDA_plots.py
    added dest_path

--- src/EDA_plots.py
+++ src/EDA_plots.py
@@ -13,20 +13,22 @@ def EDA_plot(train_df, hist_output, boxplot_output):
         include=np.number).columns.tolist()
     benign_cases = train_df[train_df["class"] == 0]
     malignant_cases = train_df[train_df["class"] == 1]
+    
     #plot histogram
     plot_hist_overlay(df0=benign_cases, df1=malignant_cases,
                  columns=numeric_looking_columns, labels=["0 - benign", "1 - malignant"],
                  fig_no="1")
-    plt.savefig(str(hist_output))
+    plt.savefig(dest_path + "hist_output") #added dest_path
+    
     #plot boxplot 
     boxplot_plotting(3, 3, 20, 25, numeric_looking_columns, train_df, 2)
-    plt.savefig(str(boxplot_output))
+    plt.savefig(dest_path + "boxplot_output")) #added dest_path
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Plots EDA")
     parser.add_argument("train_df", help="Path to train_df")
-    parser.add_argument(dest_path + "hist_output", help="Path to histogram output")
-    parser.add_argument(dest_path + "boxplot_output", help="Path to boxplot output")
+    parser.add_argument("hist_output", help="Path to histogram output")
+    parser.add_argument("boxplot_output", help="Path to boxplot output")
     args = parser.parse_args()
     EDA_plot(args.train_df, args.hist_output, args.boxplot_output)
