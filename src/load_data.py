1d3e49501d2fb51b3ffb7e0700997cae8e9820e1
Heey00 <102456788+Heey00@users.noreply.github.com>
2022-03-27 16:03:22 -0700

    Update load_data.py
    Added second arg - dest_path

--- src/load_data.py
+++ src/load_data.py
@@ -13,7 +13,8 @@ def load_data(path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Load data")
-    parser.add_argument("src_path", type=str, help="Path to data source") #need two arguments
+    parser.add_argument("src_path", type=str, help="Path to data source")
+    parser.add_argument("dest_path", type=str, help="Path to data source") #need two arguments # added second arg
     args = parser.parse_args()
-    load_data(args.path)
+    load_data(args.src_path, args.dest_path)
 
