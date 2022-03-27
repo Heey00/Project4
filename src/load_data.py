55f2d158f65bae6756a6869c3899e5f4ce933c79
Heey00 <102456788+Heey00@users.noreply.github.com>
2022-03-27 15:30:24 -0700

    Update load_data.py
    Added description and delete return on line 11

--- src/load_data.py
+++ src/load_data.py
@@ -1,13 +1,14 @@
 import argparse
 import pandas as pd
 
+## Load data from data directory and adds column names. Remove "col_names" code if data has column names
 
 def load_data(path):
     col_names = ["id", "clump", "unif_size", "unif_shape", "adhesion",
                  "epi_size", "nuclei", "chromatin", "nucleoli",
                  "mitoses", "class"]
     dataset = pd.read_csv(path, names=col_names, sep=",")
-    return dataset.to_csv('../data/raw/breast_cancer.csv')
+    dataset.to_csv('../data/raw/breast_cancer_loaded.csv') #deleted return 
 
 
 if __name__ == '__main__':
