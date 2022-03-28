cbea9941f8b211eb5ddd5b2077f8526b4633d223
Heey00 <102456788+Heey00@users.noreply.github.com>
2022-03-27 17:04:41 -0700

    Update run_all.sh
    Changed MakeFile

--- src/run_all.sh
+++ src/run_all.sh
@@ -1,8 +1,8 @@
 # load data
-python load_data.py "../data/raw/breast_cancer.txt"
+python load_data.py "../data/raw/breast_cancer.txt" "../data/breast_cancer_loaded.csv
 
 # clean data
-python clean_data.py "../data/raw/breast_cancer.csv"
+python clean_data.py "../data/breast_cancer_loaded.csv" "../data/train_df.csv"  "../data/test_df.csv"
 
 # plot EDA
 python EDA_plots.py "../data/processed/train_df.csv"
