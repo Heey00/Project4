b61f3e0b9832d732b61f48defdc8ebfc595233f4
Heey00 <102456788+Heey00@users.noreply.github.com>
2022-03-27 17:10:08 -0700

    Update run_all.sh

--- src/run_all.sh
+++ src/run_all.sh
@@ -8,7 +8,7 @@ python clean_data.py "../data/breast_cancer_loaded.csv" "../data/train_df.csv"  
 python EDA_plots.py "../data/train_df.csv" "../results/hist_output.png" "../results/boxplot_output.png"
 
 # build and test models
-python build_test_model.py "../data/train_df.csv" "../data/test_df.csv" ""../results/"
+python build_test_model.py "../data/train_df.csv" "../data/test_df.csv" "../results/"
 
 # render report
 jupyter-book build "../report/
