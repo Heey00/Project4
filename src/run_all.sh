6fa1934733bd88f558cf9218ec9b8977ca28a60f
Heey00 <102456788+Heey00@users.noreply.github.com>
2022-03-27 17:09:32 -0700

    Update run_all.sh

--- src/run_all.sh
+++ src/run_all.sh
@@ -5,10 +5,10 @@ python load_data.py "../data/raw/breast_cancer.txt" "../data/breast_cancer_loade
 python clean_data.py "../data/breast_cancer_loaded.csv" "../data/train_df.csv"  "../data/test_df.csv"
 
 # plot EDA
-python EDA_plots.py "../data/processed/train_df.csv"
+python EDA_plots.py "../data/train_df.csv" "../results/hist_output.png" "../results/boxplot_output.png"
 
 # build and test models
-python build_test_model.py "../data/processed/train_df.csv" "../data/processed/test_df.csv"
+python build_test_model.py "../data/train_df.csv" "../data/test_df.csv" ""../results/"
 
 # render report
 jupyter-book build "../report/
